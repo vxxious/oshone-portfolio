6ce62241241c7080d58919834536ed9d47ac97e2
Oshone Omoh <omohoshone5@gmail.com>
2026-07-12 21:05:19 +0100

    Fix CV download path

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -98,7 +98,7 @@ const Index = () => {
             Online
           </div>
         </div>
-        <a href="/Oshone-Omoh-cv.pdf" download className="cv-button">
+        <a href="/oshone-omoh-cv.pdf" download className="cv-button">
           Download my CV
           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
